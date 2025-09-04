1b0e000598a75e55be9aa7b50868132f8e08e62c
kienth <kien.th@lienketdachieu.com>
2025-09-04 11:21:40 +0700

    Update cookie expiration dates and values in cookieData.js; remove deprecated cookies for cleaner data structure.

--- cookieData.js
+++ cookieData.js
@@ -1,7 +1,7 @@
 const cookieData = [
     {
         "domain": ".zalo.me",
-        "expirationDate": 1752813946.395707,
+        "expirationDate": 1757043855.9959,
         "hostOnly": false,
         "httpOnly": false,
         "name": "_zlang",
@@ -14,7 +14,7 @@ const cookieData = [
     },
     {
         "domain": ".chat.zalo.me",
-        "expirationDate": 1753332346.31784,
+        "expirationDate": 1757562255.868683,
         "hostOnly": false,
         "httpOnly": true,
         "name": "zpw_sek",
@@ -23,11 +23,11 @@ const cookieData = [
         "secure": true,
         "session": false,
         "storeId": null,
-        "value": "UYSk.440367783.a0.8OtG-78XBtVLMn1jKo4lTma3IXnG6mqH5t5gGJ535YP6INmL9N0P8I9GRXqZ719L3rTfj4XdFlCk8DO82aalTm"
+        "value": "GpM2.440367783.a0.HVTsDnBDJvkrjt21CyrFcsdlAl0mzst-UeG-wNIz7zLPz1YqQg8P_rYc0y5by7AvR-SvWPHANDzTp42GgwLFcm"
     },
     {
         "domain": ".zalo.me",
-        "expirationDate": 1787287546.48541,
+        "expirationDate": 1791517456.071917,
         "hostOnly": false,
         "httpOnly": false,
         "name": "__zi",
@@ -36,37 +36,11 @@ const cookieData = [
         "secure": true,
         "session": false,
         "storeId": null,
-        "value": "3000.SSZzejyD6zOgdh2mtnLQWYQN_RAG01ICFjIXe9fEM8WwcUwYcKbJZN6VuQFIIbQ9TfdXh34t.1"
+        "value": "3000.SSZzejyD6zOgdh2mtnLQWYQN_RAG01ICFjIXe9fEM8WwcUwYcKbJZN6VuQJGJ5QAVPdhhZam.1"
     },
     {
         "domain": ".zalo.me",
-        "expirationDate": 1753238542.27069,
-        "hostOnly": false,
-        "httpOnly": true,
-        "name": "zoaw_sek",
-        "path": "/",
-        "sameSite": "no_restriction",
-        "secure": true,
-        "session": false,
-        "storeId": null,
-        "value": "a5Ow.1214459989.2.27zDr0TfMYDIMJbh1sbQ8mTfMYC7QLeZ1XpbKszfMYC"
-    },
-    {
-        "domain": ".zalo.me",
-        "expirationDate": 1753238542.270764,
-        "hostOnly": false,
-        "httpOnly": false,
-        "name": "zoaw_type",
-        "path": "/",
-        "sameSite": "no_restriction",
-        "secure": true,
-        "session": false,
-        "storeId": null,
-        "value": "0"
-    },
-    {
-        "domain": ".zalo.me",
-        "expirationDate": 1784263546.185235,
+        "expirationDate": 1788493455.580307,
         "hostOnly": false,
         "httpOnly": true,
         "name": "zpsid",
@@ -75,7 +49,7 @@ const cookieData = [
         "secure": true,
         "session": false,
         "storeId": null,
-        "value": "ZZns.440367783.1.gpqAJdISATRJvXRCU9mfrW7lM-90h1hWGwiHvGBQwkmoZQsATMLMhXoSATO"
+        "value": "GaLg.440367783.3.C9PgRC-gak1eqAtwmwgIuBhPuDJxcg7M-9sgq-NSY6P8kTQlpgMPMQUgak0"
     }
 ]
 
